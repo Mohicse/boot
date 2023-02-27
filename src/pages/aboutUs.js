d50a8261054339ac6cf7e075199b3d1833c45449
Mohit <mohit.k@theswipewire.com>
2023-02-27 12:54:32 +0530

    about us h1 changeabout us h1 change

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -2,7 +2,7 @@ const aboutUs = () => {
 
     return (
         <div className="alert alert-primary" role="alert">
-            <h1>About Us Page Journey</h1>
+            <h1>About Us Page Journey To The West</h1>
         </div>
     );
 }
